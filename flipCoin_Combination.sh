6cded38fef3139924047a35f133314b1446863fa
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 10:22:01 +0530

    added code for flipping coin an storing singlet combination in dictionary and finding it's percentage

--- flipCoin_Combination.sh
+++ flipCoin_Combination.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-toss=$(( RANDOM % 2 ))
-if(( toss == 0 ))
-then
-    echo "Heads"
-else
-    echo "Tails"
-fi 
+read -p "Enter no of times to flip single coin:" n
+declare -A dictionary=( [heads]=0 [tails]=0 )
+for(( flip = 1; flip <= n; flip++ ))
+do
+    echo -n "Flip-$flip is "
+    (( toss = RANDOM % 2 ))
+    if(( toss == 0 ))
+    then
+        echo "Heads"
+        (( dictionary[heads]++ ))
+    else
+        echo "Tails"
+        (( dictionary[tails]++ ))
+    fi
+done
+
+for combination in ${!dictionary[@]}
+do
+    count=${dictionary[$combination]}
+    percentage=$( echo "$count $n" | awk '{printf "%.2f", $1 * 100 / $2}')
+    echo "percentage of $combination is $percentage"   
+done 
